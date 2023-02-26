0c62fa65a74fd6045d07ef3408a467481a2ab02d
Ronny Mandal <ronnyma@transfinite.io>
2023-02-26 15:55:26 +0100

    Sequential stream, not parallel

--- src/main/java/io/transfinite/ConsecutiveElements.java
+++ src/main/java/io/transfinite/ConsecutiveElements.java
@@ -1,4 +1,4 @@
-package org.example;
+package io.transfinite;
 
 import java.util.ArrayList;
 import java.util.Collections;
--- src/main/java/io/transfinite/Main.java
+++ src/main/java/io/transfinite/Main.java
@@ -1,20 +1,20 @@
-package org.example;
+package io.transfinite;
 
 import java.util.List;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 
 public class Main {
 
     public static void main(String[] args) {
         List<Character> characters = List.of('a', 'b', 'c', 'c', 'c', 'd', 'd');
-        List<Character> characters2 = List.of('c', 'd', 'e','e');
+        List<Character> characters2 = List.of('c', 'd', 'e', 'e', 'd');
 
         listConsecutiveDuplicates(characters);
         listConsecutiveDuplicates(characters2);
     }
 
     private static <T> void listConsecutiveDuplicates(List<T> list) {
-        List<T> o = list.parallelStream()
+        List<T> o = list.stream()
             .collect(new ConsecutiveElements<>());
 
         System.out.println("Duplicate consecutive element [" + StringUtils.join(list, ",") + "]: ");
