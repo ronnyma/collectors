3a0c4feb6c5b43679cf32a0993b5f4f19d8710a0
Ronny Mandal <ronnyma@transfinite.io>
2023-03-05 18:12:15 +0100

    Stream instead of list
    No need to make a list, then stream it.
    Correct scope for the AssertJ

--- src/test/java/io/transfinite/DuplicateAndConsecutiveElementsTest.java
+++ src/test/java/io/transfinite/DuplicateAndConsecutiveElementsTest.java
@@ -4,29 +4,30 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 import io.transfinite.collectors.DuplicateConsecutiveElements;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class DuplicateAndConsecutiveElementsTest {
 
-    List<Character> characters1;
-    List<Character> characters2;
+    Stream<Character> characters1;
+    Stream<Character> characters2;
 
     @BeforeEach
     public void setUp() {
-        characters1 = List.of('a', 'b', 'c', 'c', 'c', 'd');
-        characters2 = List.of('c', 'd', 'e', 'e', 'd');
+        characters1 = Stream.of('a', 'b', 'c', 'c', 'c', 'd');
+        characters2 = Stream.of('c', 'd', 'e', 'e', 'd');
     }
 
     @Test
     void shouldListDuplicateElements() {
-        assertThat(List.of('c')).hasSameElementsAs(characters1.stream()
+        assertThat(List.of('c')).hasSameElementsAs(characters1
             .collect(new DuplicateConsecutiveElements<>()));
     }
 
     @Test
     void shouldListDuplicateElement() {
-        assertThat(List.of('e')).hasSameElementsAs(characters2.stream()
+        assertThat(List.of('e')).hasSameElementsAs(characters2
             .collect(new DuplicateConsecutiveElements<>()));
     }
 }
--- src/test/java/io/transfinite/DuplicateElementsTest.java
+++ src/test/java/io/transfinite/DuplicateElementsTest.java
@@ -4,29 +4,30 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 import io.transfinite.collectors.DuplicateElements;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class DuplicateElementsTest {
 
-    List<Character> characters1;
-    List<Character> characters2;
+    Stream<Character> characters1;
+    Stream<Character> characters2;
 
     @BeforeEach
     public void setUp() {
-        characters1 = List.of('a', 'b', 'c', 'c', 'c', 'd', 'd');
-        characters2 = List.of('c', 'd', 'e', 'e', 'd');
+        characters1 = Stream.of('a', 'b', 'c', 'c', 'c', 'd', 'd');
+        characters2 = Stream.of('c', 'd', 'e', 'e', 'd');
     }
 
     @Test
     void shouldListDuplicateElements() {
-        assertThat(characters1.stream()
+        assertThat(characters1
             .collect(new DuplicateElements<>())).hasSameElementsAs(List.of('c', 'd'));
     }
 
     @Test
     void shouldListDuplicateElement() {
-        assertThat(characters2.stream()
+        assertThat(characters2
             .collect(new DuplicateElements<>())).hasSameElementsAs(List.of('d', 'e'));
     }
 }
